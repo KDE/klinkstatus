faa8f40897985dd9bc40532f790aad0519a4bbdd
Paulo Moura Guedes <moura@kdewebdev.org>
2004-05-10 13:19:41 +0000

    Remove STL
    svn path=/trunk/kdewebdev/klinkstatus/; revision=310032

--- src/parser/http.cpp
+++ src/parser/http.cpp
@@ -19,13 +19,11 @@
  ***************************************************************************/
 
 #include "http.h"
+#include "mstring.h"
 
 #include <cassert>
 #include <iostream>
 
-#include "mstring.h"
-
-using namespace std;
 
 
 void HttpResponseHeader::parseLocation()
@@ -56,60 +54,3 @@ void HttpResponseHeader::parseLocation()
   location_ = cabecalho.mid(location, fim_de_linha - location);
 }
 
-
-#ifdef HTTPRESPONSEHEADER
-// c++ -g -o testehttp http.C string.C -I/$QTDIR/include -L/$QTDIR/lib -lqt-mt -DHTTPRESPONSEHEADER
-
-#include <string>
-
-string parseLocation(string cabecalho)
-{
-  cerr << cabecalho << endl;
-
-  int location = findWord(cabecalho, "Location: ");  
-//  int location = cabecalho.find("Location: ");
-  assert(location != -1);
-
-  int fim_de_linha_1 = cabecalho.find("\n", location);
-  int fim_de_linha_2 = cabecalho.find("\r", location);
-
-  assert(fim_de_linha_1 != -1 || fim_de_linha_2 != -1);
-
-  int fim_de_linha;
-  
-  if(fim_de_linha_1 == -1 && fim_de_linha_2 != -1) 
-    fim_de_linha = fim_de_linha_2;
-  
-  else if(fim_de_linha_2 == -1 && fim_de_linha_1 != -1) 
-    fim_de_linha = fim_de_linha_1;
-  
-  else if(fim_de_linha_1 < fim_de_linha_2) 
-    fim_de_linha = fim_de_linha_1;
-
-  else fim_de_linha = fim_de_linha_2;
-
-  string location_ = cabecalho.substr(location, fim_de_linha - location);
-
-  cerr << "Location: " << location_ << endl;  
-
-  return location_;
-}
-
-#include <fstream>
-int main()
-{
-  ifstream stream("parselocation.html");
-  string content;
-  while(stream) {
-    char c;
-    stream.get(c);
-    content += c;
-  }
-//  cout << content << endl;
-
-  parseLocation(content);
-  
-}
-
-
-#endif
