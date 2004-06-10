4d40ed0aa5a52d6c5866db864ac937a86e6ed1d7
Paulo Moura Guedes <moura@kdewebdev.org>
2004-06-10 21:35:51 +0000

    Update release version and email address.
    svn path=/trunk/kdewebdev/doc/klinkstatus/; revision=319498

--- src/klinkstatus.h
+++ src/klinkstatus.h
@@ -36,7 +36,7 @@ class KToggleAction;
  *
  * @short Application Shell
  * @author Paulo Moura Guedes <moura@kdewebdev.org>
- * @version 0.1.0
+ * @version 0.1.3
  */
 class KLinkStatus : public KParts::MainWindow
 {
