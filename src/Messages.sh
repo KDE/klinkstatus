82ad0496e5d7565299d61f49807d81c3cb4ab9ee
Burkhard Lück <lueck@hube-lueck.de>
2011-08-23 17:29:57 +0000

    add missing i18n + make menu string from scripts.rc translatable
    svn path=/trunk/KDE/kdewebdev/klinkstatus/; revision=1249085

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,6 +3,7 @@ LIST=`find . -name \*.ui -o -name \*.kcfg -o -name \*.rc | grep -v plugins/scrip
 if test -n "$LIST"; then 
 	$EXTRACTRC $LIST >> rc.cpp 
 fi
+$EXTRACTATTR --attr=script,text --attr=script,comment plugins/scripting/scripts/scripts.rc >> rc.cpp
 LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then
 	$XGETTEXT $LIST -o $podir/klinkstatus.pot
